02faf347a05fe03c273f5b0459ede2b9c9c8cdb6
keith alleman <keith.alleman@icloud.com>
2020-10-13 07:31:38 -0600

    Moved all form fields to either TextField or SelectField reusable components. Changed binds to bind directly to the store. Submit handler will validate with schema, then we will route or display the error.

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,12 @@
 import { writable } from "svelte/store";
 import type { Monitors } from "./types";
 
-export const contact = writable({});
+export const contact = writable({
+  name: "",
+  ref: "",
+  email: "",
+  count: 1,
+});
 
 export const results = writable<Monitors>({
   monitor: [],
